ca37d8fabc378226a4dd08bec5a31dc700f7a4b8
Slava Blazhev <vsblazhev@yandex.com>
2023-03-17 20:03:21 +0100

    syntax fix

--- models/user.js
+++ models/user.js
@@ -4,19 +4,19 @@ const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
-        minlenght:2,
-        maxlength:30
+        minlength: 2,
+        maxlength: 30
     },
     about: {
         type: String,
         required: true,
-        minlenght:2,
-        maxlength:30
+        minlength: 2,
+        maxlength: 30
     },
     avatar: {
         type: String,
         required:true
-    }
+    },
 })
 
 module.exports = mongoose.model('user', userSchema) 
