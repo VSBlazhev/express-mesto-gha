ba2a31a0f1db5d0dbfe3c7439a2bf0b6bd24411c
Slava Blazhev <vsblazhev@yandex.com>
2023-03-26 19:25:47 +0200

    .

--- controllers/user.js
+++ controllers/user.js
@@ -103,7 +103,7 @@ module.exports.loginUser = (req,res) => {
       }
       const token = jwt.sign({_id: user._id}, 'key')
 
-      res.cookie('jwt', token,{ maxAge: 3600000 * 24 * 7, httpOnly: true }).end()
+      res.cookie('jwt', token,{ maxAge: 3600000 * 24 * 7, httpOnly: true }).send({token})
     })
 }
 
