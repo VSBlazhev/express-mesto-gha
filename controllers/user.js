6598be7952853ae47a3bc9df9a029e0242239f51
Slava Blazhev <vsblazhev@yandex.com>
2023-04-04 15:37:54 +0200

    test push 3

--- controllers/user.js
+++ controllers/user.js
@@ -61,7 +61,7 @@ module.exports.loginUser = (req, res, next) => {
   User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, 'key');
-      return res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true }).end();
+      return res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true }).send({message: 'Вы вошли'});
     })
     .catch(next);
 };
